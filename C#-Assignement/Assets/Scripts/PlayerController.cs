5496f462889e36f48e4d5e639cb5f8c72934c4d1
Lucas-Lundin <113420344+Lucas-Lundin@users.noreply.github.com>
2022-09-14 16:31:55 +0200

    Basic primitives and folders setup

--- C#-Assignement/Assets/Scripts/PlayerController.cs
+++ C#-Assignement/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 15f;
+    [SerializeField] private float boostDirctionChange = 15f;
+    private float xInput;
+    private float zInput;
+    public Rigidbody thisRigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void Awake()
+    {
+        thisRigidBody = GetComponent<Rigidbody>();
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessInput(); 
     }
+
+    private void FixedUpdate()
+    {
+        // Movment
+        Move();
+    }
+
+    // Checks for inputs
+    private void ProcessInput()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+    }
+
+    private void Move()
+    {
+        Debug.Log("X Vel: " + thisRigidBody.velocity.x + " xInput: " + xInput);
+
+        // Boosting force when changing Horizontal direction
+        float boostXDirctionChange = 1f;
+        if ((thisRigidBody.velocity.x < 0 && xInput > 0) || (thisRigidBody.velocity.x > 0 && xInput < 0))
+        {
+            boostXDirctionChange = boostDirctionChange;
+        }
+
+        // Boosting force when changing Vertical direction
+        float boostZDirctionChange = 1f;
+        if ((thisRigidBody.velocity.z < 0 && zInput > 0) || (thisRigidBody.velocity.z > 0 && zInput < 0))
+        {
+            boostZDirctionChange = boostDirctionChange;
+        }
+
+
+        thisRigidBody.AddForce(new Vector3(xInput * boostXDirctionChange, 0f, zInput * boostZDirctionChange) * moveSpeed);
+
+
+        //thisRigidBody.AddForce(new Vector3(xInput, 0f, zInput) * moveSpeed);
+    }
+      
+
 }
